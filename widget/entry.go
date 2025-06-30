aa0e98ac80f9422dcf31dd89b52240d41860199a
Hayden Parker <hayden@hkparker.com>
2025-06-30 05:39:42 -0600

    add entry's action item to the renderer's objects if it not nil

--- widget/entry.go
+++ widget/entry.go
@@ -1638,7 +1638,11 @@ func (r *entryRenderer) MinSize() fyne.Size {
 }
 
 func (r *entryRenderer) Objects() []fyne.CanvasObject {
-	return r.objects
+	objs := r.objects
+	if r.entry.ActionItem != nil {
+		objs = append(objs, r.entry.ActionItem)
+	}
+	return objs
 }
 
 func (r *entryRenderer) Refresh() {
